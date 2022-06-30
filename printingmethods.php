6822a27be7b5b1d711b1cd7f9da174c4ac2a0d3d
ishaparmar9 <101660450+ishaparmar9@users.noreply.github.com>
2022-06-30 15:33:10 +0530

    Add files via upload

---
+++ printingmethods.php
@@ -0,0 +1,25 @@
+<?php
+    echo "Welcome to the world of php";
+
+    $en = 190320107026;
+    $name = "ISHA";
+
+    // method1
+    echo "<br/>Hello ".$name;
+    echo ", Enrollment No.: ".$en;
+
+    echo "<br/><br/>";
+
+    //method2
+    echo "<br/>Hello ".$name.", Enrollment No.: ".$en;
+
+    echo "<br/><br/>";
+
+    //method3
+    echo "<br/>Hello $name, Enrollment No.: $en";
+
+
+    echo "<br/><br/><br/>";
+    echo '<br/>Hello $name, Enrollment No.: $en';
+
+?>
